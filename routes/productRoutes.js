0db5be7c964e7efaa466d2764ee8583ae660d2bf
parksungchul <parksungchul@Sungchul-MacBook-Pro.local>
2021-05-06 15:46:38 -0400

    save products to my cart

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -4,6 +4,7 @@ const productController = require('../controllers/productController')
 
 productRoutes.get('/', productController.getAllProducts)
 productRoutes.get('/:id', productController.getOneProduct)
+productRoutes.post('/:id', productController.save)
 
 
 
