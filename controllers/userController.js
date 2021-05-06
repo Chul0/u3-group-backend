ebe575e6c207334cda2db21e067ee74f3f285b36
parksungchul <parksungchul@Sungchul-MacBook-Pro.local>
2021-05-05 22:48:07 -0400

    user controller/routes

---
+++ controllers/userController.js
@@ -0,0 +1,76 @@
+require('dotenv').config()
+const jwt = require('jsonwebtoken')
+const bcrypt = require('bcrypt')
+
+const models = require('../models')
+
+
+const  userController ={}
+
+//signup
+userController.create = async (req, res) => {
+    try {
+        const hashedPassword = bcrypt.hashSync(req.body.password, 10)
+
+        const user = await models.user.create({
+            email: req.body.email,
+            password: hashedPassword
+        })
+
+        const encryptedId = jwt.sign({ userId: user.id}, process.env.JWT_SECRET)
+        
+
+        res.json({user, message: 'User created!', userId: encryptedId})
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({error: error.message})
+    }
+}
+
+//login
+
+userController.login = async(req, res) => {
+    try {
+      const user = await models.user.findOne({
+        where: { 
+            email: req.body.email 
+        }
+      })
+  
+      if (bcrypt.compareSync(req.body.password, user.password)) {
+        const encryptedId = jwt.sign({ userId: user.id}, process.env.JWT_SECRET)
+
+        res.json({ user, message: 'login successful', userId:encryptedId })
+      } else {
+        res.status(401).json({ message: 'login failed' })
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(400).json({ error: error.message })    
+    }
+  }
+
+  userController.verifyUser = async (req, res) => {
+    try {
+      const decryptedId = jwt.verify(req.headers.authorization, process.env.JWT_SECRET)
+      
+      const user = await models.user.findOne({
+        where: {
+          
+          id: decryptedId.userId
+        }
+      })
+  
+      if (user) {
+        res.json({user, message:'user found'})
+      }else {
+        res.status(404).json({message: 'user not found'})
+      }
+    } catch (error) {
+      console.log(error);
+      res.status(400).json({error: error.message})
+    }
+  }
+
+
+  module.exports = userController
